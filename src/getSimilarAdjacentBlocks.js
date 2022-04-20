092a9e0c31476badea416d3562f96067f65dee1f
Darin Kadrioski <darin@studysync.com>
2022-04-20 13:18:29 -0700

    adding getSimilarAdjacentBlocks

---
+++ src/getSimilarAdjacentBlocks.js
@@ -0,0 +1,44 @@
+const getSimilarAdjacentBlocks = (editorState, block) => {
+  const blockType = block.getType()
+  const depth = block.getDepth()
+  const variant = block.getData().get('variant')
+
+  const contentState = editorState.getCurrentContent()
+
+  const matches = aBlock => (aBlock.getType() === blockType)
+    && (aBlock.getDepth() === depth)
+    && (aBlock.getData().get('variant') === variant)
+
+  const before = []
+  const after = []
+
+  let cursor = block
+  while (cursor) {
+    const prev = contentState.getBlockBefore(cursor.getKey())
+    if (matches(prev)) {
+      before.push(prev)
+      cursor = prev
+    } else {
+      cursor = null
+    }
+  }
+
+  cursor = block
+  while (cursor) {
+    const next = contentState.getBlockAfter(cursor.getKey())
+    if (matches(next)) {
+      after.push(next)
+      cursor = next
+    } else {
+      cursor = null
+    }
+  }
+
+  return {
+    before: before.reverse(),
+    after,
+    all: [...before.reverse(), block, ...after],
+  }
+}
+
+export default getSimilarAdjacentBlocks
